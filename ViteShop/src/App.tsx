9988de2e7e93103f80ea4d72b81085df151c17ae
Minh <86398860+minhnnhob@users.noreply.github.com>
2024-07-09 03:18:33 +0700

    Comming Soon

--- ViteShop/src/App.tsx
+++ ViteShop/src/App.tsx
@@ -1,32 +1,26 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Comming Soon</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-         fdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-        </p>
       </div>
-      <p className="read-the-docs">
- fdsfdsfsdfdssssssssssssssssssseeee
-      </p>
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4OTpMSw0dyLk2vahdpl2juD-95MV3el9vnQ&s"
+          alt=""
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
